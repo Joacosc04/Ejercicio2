0194a80b1661631fe2f6a5fafdcb0e83662d4094
Joacosc04 <joakinsc04@gmail.com>
2022-08-18 18:55:30 -0300

    dos

--- src/com/openbootcamp/Main.java
+++ src/com/openbootcamp/Main.java
@@ -1,10 +1,15 @@
 package com.openbootcamp;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println(precioConIVA(265));
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Calcular el IVA de un precio");
+        int precio = scanner.nextInt();
+        System.out.println(precioConIVA(precio));
     }
-    public static int precioConIVA(int precio){
+    public static double precioConIVA(int precio){
         return (precio/100) * 121;
     }
 }
